482496eb651dff64cf6fef74a5881566077880fb
Kunal Pal <kunal.pal1010@gmail.com>
2024-07-31 19:07:52 +0530

    Different RAG Retrievers

---
+++ basic_rag_retriever.py
@@ -0,0 +1,92 @@
+import os
+
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+
+model_name = "BAAI/bge-base-en-v1.5"
+model_kwargs = {"device": "cpu"}
+encode_kwargs = {"normalize_embeddings": True}
+
+# define Path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+db_dir = os.path.join(current_dir, "db")
+persistent_directory = os.path.join(db_dir, "chroma_db_with_metadata")
+
+# loading embeddings
+
+embeddings = HuggingFaceBgeEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs
+    ) 
+
+#load the existing vector database
+db = Chroma(persist_directory=persistent_directory, 
+            embedding_function=embeddings)
+
+# Function for querying a vector store using different search parameters 
+def query_vector_store(store_name, query, embedding_function, search_type, search_kwargs):
+    if os.path.exists(persistent_directory):
+        print(f"\n--- Querying Vector Store > {store_name} < 🔄 ---") 
+        db = Chroma(
+            persist_directory=persistent_directory, embedding_function= embeddings
+        )
+        retriever = db.as_retriever(
+            search_type = search_type,
+            search_kwargs = search_kwargs 
+        )
+        relevant_docs = retriever.invoke(query)
+        # Display The relevant results with metadata
+        print(f"\n--- Relevant Documents For > {store_name} <  ---")
+        for i, doc in enumerate(relevant_docs, 1):
+            print(f"Document {i}: \n {doc.page_content}\n")
+            if doc.metadata:
+                print(f"Source: {doc.metadata.get('source','Unknown')}\n")
+    else:
+        print(f"Vector Store {store_name} does not exist 🔴")  
+
+# User Query
+query = "What is python's use ?"
+
+# Showcase different retrieval methods
+
+# 1. Similarity Search
+# This method retrieves documents based on vector similarity.
+# It finds the most similar documents to the query vector based on cosine similarity.
+# Use this when you want to retrieve the top k most similar documents.
+print("\n--- Using Similarity Search ---")
+query_vector_store("chroma_db_with_metadata", query,
+                   embeddings, "similarity", {"k": 3})
+
+# 2. Max Marginal Relevance (MMR)
+# This method balances between selecting documents that are relevant to the query and diverse among themselves.
+# 'fetch_k' specifies the number of documents to initially fetch based on similarity.
+# 'lambda_mult' controls the diversity of the results: 1 for minimum diversity, 0 for maximum.
+# Use this when you want to avoid redundancy and retrieve diverse yet relevant documents.
+# Note: Relevance measures how closely documents match the query.
+# Note: Diversity ensures that the retrieved documents are not too similar to each other,
+#       providing a broader range of information.
+print("\n--- Using Max Marginal Relevance (MMR) ---")
+query_vector_store(
+    "chroma_db_with_metadata",
+    query,
+    embeddings,
+    "mmr",
+    {"k": 3, "fetch_k": 20, "lambda_mult": 0.5},
+)
+
+# 3. Similarity Score Threshold
+# This method retrieves documents that exceed a certain similarity score threshold.
+# 'score_threshold' sets the minimum similarity score a document must have to be considered relevant.
+# Use this when you want to ensure that only highly relevant documents are retrieved, filtering out less relevant ones.
+print("\n--- Using Similarity Score Threshold ---")
+query_vector_store(
+    "chroma_db_with_metadata",
+    query,
+    embeddings,
+    "similarity_score_threshold",
+    {"k": 3, "score_threshold": 0.1},
+)
+
+print("Querying demonstrations with different search types completed.")
